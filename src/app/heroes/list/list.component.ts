a71a75d0ac0663931cc92d29ab494722acba70f7
Esteban Salas <estebansalas2502@gmail.com>
2024-04-29 20:54:00 -0600

    Seccion #4

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+  public heroNames: string[] = [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'Thor',
+    'Captain America',
+    'Black Widow',
+    'Hawkeye',
+    'Doctor Strange',
+    'Black Panther',
+    'Antman',
+    'Wasp',
+    'Captain Marvel',
+    'Scarlet Witch',
+  ];
+  public deletedHero?: string = '';
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+  }
+}
